f3d2e7cfdfe43440b02cd8ae139b04ac64e08ea6
Sebas <sebastiancalvom@gmail.com>
2019-09-24 12:13:55 +0200

    WIP: Script python para obtener datos de repositorios en GH

--- python/get_repos.py
+++ python/get_repos.py
@@ -1,30 +1,29 @@
-import scrapy
+#!/usr/bin/python3
+from github import Github
+
+import sys
 
 
-class GitHubSpider(scrapy.Spider):
-    name = 'github_spider'
-    start_urls = ['https://github.com/search?o=desc&q=followers%3A%3E%3D38000&s=updated&type=Repositories']
-
-    def parse(self, response):
-       """ Main function that parses downloaded pages """
-       # Print what the spider is doing
-       print(response.url)
-       # Get all the <a> tags
-       a_selectors = response.xpath('//ul[@class="repo-list"]/li//h3/a/@href').extract()
-       print('############################################')
-       print(a_selectors)
-       # Loop on each tag
-       #for li in a_selectors:
-       ## Extract the link text
-       #     href = li.xpath("/h3/a/#href").extract_first()
-       ##    # Extract the link href
-       ##    link = selector.xpath("@href").extract_first()
-       ##    # Create a new Request object
-       ##    #request = response.follow(link, callback=self.parse)
-       ##    # Return it thanks to a generator
-       next_page_url = response.xpath('//a[@class="next_page"]/@href').extract_first()
-       next_page_url = response.urljoin(next_page_url)
-       yield scrapy.Request(url = next_page_url, callback = self.parse)
+class Connection():
+    def __init__(self):
+        self.token = sys.argv[1]
+        self.repo = sys.argv[2]
+        self.connection = Github(self.token)
 
 
-    
+class Commits(Connection):
+    connection = Connection()
+    g = connection.connection
+    repo = connection.repo
+    totalCommits = g.get_repo(repo).get_commits().totalCount
+    commits = g.get_repo(repo).get_commits()
+    totalContributors = g.get_repo(repo).get_contributors().totalCount
+    contributors = g.get_repo(repo).get_contributors()
+    listCommits = []
+    for commit in commits:
+            if (commit is None or commit.author is None or commit.author.login
+                    is None):
+                listCommits.append('None')
+            else:
+                listCommits.append(commit.author.login)
+    print(listCommits)
