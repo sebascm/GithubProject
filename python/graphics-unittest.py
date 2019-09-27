69b47d48067b3afb7894e1c67faa62ea1d4b9817
losete <losetazo@gmail.com>
2019-09-27 13:20:09 +0200

    Eliminados comentarios de los tests

--- python/graphics-unittest.py
+++ python/graphics-unittest.py
@@ -2,12 +2,13 @@
 import graphics
 import os
 
-def testloadScheme(dir):
-	scheme1 = { "user": ("string", "User name"),
-    	            "commits": ("number", "Number of user commits")}
-   	print "hola"
-	result1 = graphics.loadScheme(os.path.join(dir, "schema1"))
-	assertEqual(result1, scheme1)
+class TestSum(unittest.TestCase):
+
+	def testloadScheme(dir):
+		scheme1 = { "user": ("string", "User name"),
+	    	            "commits": ("number", "Number of user commits")}
+		result1 = graphics.loadScheme(os.path.join(dir, "schema1"))
+		assertEqual(result1, scheme1)
 
 def main():
 	rootdir = os.path.join(os.getcwd(), "test/schemas")
