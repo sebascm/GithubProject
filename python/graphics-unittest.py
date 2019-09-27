ab7e32b36d0987d865f5fa5497071b7534f17445
losete <losetazo@gmail.com>
2019-09-27 14:18:12 +0200

    Pequeños cambios para cambio a python3

--- python/graphics-unittest.py
+++ python/graphics-unittest.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python
+import unittest
 import graphics
 import os
 
 
-def testloadScheme(dir):
-    scheme1 = {"user": ("string", "User name"),
-               "commits": ("number", "Number of user commits")}
-    result1 = graphics.loadScheme(os.path.join(dir, "schema1"))
-    # Hecho rapido para que pep8 no de la lata
-    if (scheme1 == result1):
-        print "Bien"
-
-
-def main():
-    rootdir = os.path.join(os.getcwd(), "test/schemas")
-    testloadScheme(rootdir)
+class Test(unittest.TestCase):
+    def testloadScheme(self):
+        rootdir = os.path.join(os.getcwd(), "tests/schemas/")
+        schema1 = {"user": ("string", "User name"),
+                   "commits": ("number", "Number of user commits")}
+        result1 = graphics.loadScheme(os.path.join(rootdir, 'schema1'))
+        self.assertEqual(result1, schema1)
+        schema2 = {'day': ('string', 'Day'),
+                   'proy1': ('number', 'Commits proyecto 1'),
+                   'proy2': ('number', 'Commits proyecto 2'),
+                   'proy3': ('number', 'Commits proyecto 3')}
+        result2 = graphics.loadScheme(os.path.join(rootdir, 'schema2'))
+        self.assertEqual(result2, schema2)
 
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
