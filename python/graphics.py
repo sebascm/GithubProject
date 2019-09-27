cb8b321f9aace7052577ebe1d0598314b22f10ac
losete <losetazo@gmail.com>
2019-09-27 11:04:27 +0200

    Se añaden automáticamente scripts de ejecución de gráficos y divs

--- python/graphics.py
+++ python/graphics.py
@@ -33,11 +33,15 @@ def loadScheme(file):
 
 
 def main():
+    jscode = ''
+    ejecucion = ''
+    opciones = ''
+    divs = ''
     rootdir = os.path.join(os.getcwd(), "data/")
 
     for subdir, dirs, files in os.walk(rootdir):
-        description = None
         data = None
+        description = None
         for file in files:
             archivo = os.path.join(subdir, file)
             if file == 'schema':
@@ -49,25 +53,29 @@ def main():
         if data is not None:
             data_table = createAndLoadDataTable(description, data['Datos'])
             grafico = str(data['Grafico'])
-            ejecucion = 'var chart = new google.visualization.' + grafico + \
-                '(document.getElementById(\'' + grafico + '_div\'));'
-            opciones = 'chart.draw(jscode_data,' + grafico + '_options);'
-            jscode = None
+            divs = divs + 'var iDiv = document.createElement(\'div\'); ' + '\n' \
+                + 'iDiv.id = \'' + grafico + '_div\';' + '\n' + \
+                'document.getElementsByTagName(\'body\')[0].appendChild(iDiv);'
+            ejecucion = ejecucion + '\n' + 'var chart_' + grafico + '= new google.visualization.' + \
+                grafico + '(document.getElementById(\'' + grafico + '_div\'));'
+            opciones = opciones + '\n' + 'chart_' + grafico + \
+                '.draw(jscode_data,' + grafico + '_options);'
             if 'Ordenacion' in data:
-                jscode = data_table.ToJSCode(
+                jscode = jscode + '\n' + data_table.ToJSCode(
                     "jscode_data", columns_order=(
                         description.keys()), order_by=str(
                         data['Ordenacion']))
             else:
-                jscode = data_table.ToJSCode(
+                jscode = jscode + '\n' + data_table.ToJSCode(
                     "jscode_data", columns_order=(
                         description.keys()))
-            pagina = page_template % vars()
-            if not os.path.isdir("generated"):
-                os.mkdir("generated")
-            f = open("generated/prueba.html", "w+")
-            f.write(pagina)
-            f.close()
+
+    pagina = page_template % vars()
+    if not os.path.isdir("generated"):
+        os.mkdir("generated")
+    f = open("generated/prueba.html", "w+")
+    f.write(pagina)
+    f.close()
 
 
 if __name__ == "__main__":
